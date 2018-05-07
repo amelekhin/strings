d5fdf054491e7f51b2cbf2ba96466f62c727720c
Anton Amelekhin <a.amelekhin@outlook.com>
2018-05-07 18:42:27 +0300

    Improve run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-go build -ldflags=-w
+go build -ldflags=-w -o strings main.go
+
 ./strings -t=data/dna-text.txt -p=data/dna-pattern.txt
 ./strings -t=data/war-peace-text.txt -p=data/war-peace-pattern.txt
 ./strings -t=data/lorem-ipsum-text.txt -p=data/lorem-ipsum-pattern.txt
